890e440fceb98a41530cff9a7d958d35ba0fdb03
Frank Théodoloz <frank.theodoloz@etu.hesge.ch>
2022-06-08 14:00:23 +0200

    iot1 and 2 code

---
+++ iot1/hml/scripts.js
@@ -0,0 +1,37 @@
+const content = 'content.html';
+let lastUpdate = null;
+
+function updateContent() {
+    fetch(location + content)
+        .then(res => {
+            if (checkIsNewDate(res.headers.get('Last-Modified'))) {
+                setContent();
+            }
+        });
+}
+
+function checkIsNewDate(d) {
+    let testDate = Date.parse(d)
+    if (lastUpdate == null) {
+        lastUpdate = testDate;
+        return true
+    } else if (testDate > lastUpdate) {
+        lastUpdate = testDate;
+        return true;
+    }
+    return false;
+}
+
+function setContent() {
+    document.getElementById("content")
+        .setAttribute("data", "./content.html")
+}
+
+function init() {
+    updateContent();
+    setInterval(() => {
+        updateContent();
+    }, 5000);
+}
+
+init();
---
+++ iot2/server_udp.py
@@ -0,0 +1,34 @@
+import json
+import socket
+
+import ticker
+
+UDPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+UDPServerSocket.bind(("192.168.1.20", 12345))
+
+TICKERS = [
+    "BTC-USD",
+    "ETH-USD",
+    "XRP-USD",
+    "ADA-USD",
+]
+
+while True:
+    print("Waiting for UDP connection")
+
+    msgClient, coordClient = UDPServerSocket.recvfrom(1024)
+
+    # print("Message received from " + coordClient[0] + ":" + str(coordClient[1]) + " : " + msgClient.decode("UTF-8"))
+    UDPServerSocket.sendto(("UDP Server received : " + msgClient.decode("UTF-8")).encode('UTF-8'), coordClient)
+
+    data = msgClient.decode("UTF-8")
+    print(data)
+    if data != "":
+        if data == "list":
+            res = json.dumps(TICKERS)
+            UDPServerSocket.sendto(res.encode('UTF-8'), coordClient)
+        if data in TICKERS:
+            print("True " + data)
+            ticker.get_chart(data)
+
+UDPServerSocket.close()
