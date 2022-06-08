0a422a7748f7681292b116d96fdd3e0a794e9710
Frank Théodoloz <frank.theodoloz@etu.hesge.ch>
2022-06-08 16:52:37 +0200

    Updated M5 main

---
+++ m5/main.py
@@ -0,0 +1,51 @@
+from m5stack import lcd, buttonA, buttonB, buttonC
+import socket
+import time
+import pir
+import wifi
+
+WLAN_SSID = "MediaNet"
+WLAN_KEY = "ri0a-4a0b-lcyo-7qwb"
+SERVER_IP = "192.168.1.22"
+SERVER_PORT = 65431
+
+PIR_LASTSTATE = 0
+
+lcd.clear()
+lcd.setCursor(0, 0)
+lcd.setColor(lcd.WHITE)
+lcd.print("ButtonA=\n")
+lcd.print("ButtonC=\n")
+lcd.print("ButtonD=\n")
+
+
+def sendmsg(ip, port, message):
+    TCPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    TCPClientSocket.connect((ip, port))
+    TCPClientSocket.sendall(message.encode('UTF-8'))
+    msgServeur = TCPClientSocket.recv(1024)
+    print("[TCP] server reply: ", msgServeur.decode('UTF-8'))
+    TCPClientSocket.close()
+
+
+wifi.connect(WLAN_SSID, WLAN_KEY)
+PIR_MODULE = pir.PIR()
+
+while True:
+    time.sleep(0.5)
+    if buttonA.isPressed():
+        sendmsg(SERVER_IP, SERVER_PORT, "buttonA")
+    if buttonB.isPressed():
+        sendmsg(SERVER_IP, SERVER_PORT, "buttonB")
+    if buttonC.isPressed():
+        sendmsg(SERVER_IP, SERVER_PORT, "buttonC")
+
+    PIR_CURRENTSTATE = PIR_MODULE.read()
+    if (PIR_CURRENTSTATE != PIR_LASTSTATE):
+        if (PIR_CURRENTSTATE == 1):
+            sendmsg(SERVER_IP, SERVER_PORT, "PIR_ON")
+        if (PIR_CURRENTSTATE == 0):
+            sendmsg(SERVER_IP, SERVER_PORT, "PIR_OFF")
+        PIR_LASTSTATE = PIR_CURRENTSTATE
+    time.sleep(0.2)
+
